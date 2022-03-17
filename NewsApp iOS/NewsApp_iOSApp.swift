9edcca817d9840bae281ff62b4a2d8187283e30e
Sai Krishna <sai@surveyheart.com>
2022-03-17 15:37:06 +0530

    Initial Commit

---
+++ NewsApp iOS/NewsApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_iOSApp.swift
+//  NewsApp iOS
+//
+//  Created by sai krishna korukanti on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
